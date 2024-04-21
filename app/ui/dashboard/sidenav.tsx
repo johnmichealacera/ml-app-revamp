fc4a1b34cec3d4fb3f37c19c105c272ba5d3f6a2
JM Acera <john.acera@sportality.se>
2024-04-21 12:37:49 +0800

    code changes

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -11,7 +11,8 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+          {/* <AcmeLogo /> */}
+          Help App
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
