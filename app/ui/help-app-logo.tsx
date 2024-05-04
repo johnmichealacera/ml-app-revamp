756cb0997fb4645184b82342888e7525c315eb4a
JM Acera <john.acera@sportality.se>
2024-05-04 21:41:35 +0800

    fix naming and responsive design for sidenav in mobile

--- app/ui/help-app-logo.tsx
+++ app/ui/help-app-logo.tsx
@@ -2,18 +2,18 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import { getUserdata } from '@/auth';
 
-export default async function AcmeLogo() {
+export default async function HelpAppLogo() {
   const userdata: any = await getUserdata();
 
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white w-80 md:w-80 lg:w-48`}
     >
       <Image
         src="/socorro-logo.png"
         width={50}
         height={50}
-        className="h-12 w-12"
+        className="h-12 w-12 md:h-16 md:w-16"
         alt="Socorro logo"
       />
       {userdata?.department === 'hospital' && (
@@ -21,6 +21,7 @@ export default async function AcmeLogo() {
           src="/hospital.png"
           width={50}
           height={50}
+          className="h-12 w-12 md:h-16 md:w-16"
           alt="Hospital logo"
         />
       )}
@@ -29,6 +30,7 @@ export default async function AcmeLogo() {
           src="/police.png"
           width={50}
           height={50}
+          className="h-12 w-12 md:h-16 md:w-16"
           alt="Police logo"
         />
       )}
@@ -37,6 +39,7 @@ export default async function AcmeLogo() {
           src="/fire.png"
           width={50}
           height={50}
+          className="h-12 w-12 md:h-16 md:w-16"
           alt="Fire logo"
         />
       )}
