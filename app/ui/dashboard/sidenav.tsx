27715cc071f056c492ba2c96a4f7beb97fa2e5d2
JM Acera <john.acera@sportality.se>
2024-05-04 20:53:17 +0800

    add personnel info in side nav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '@/auth';
+import { getUserdata, signOut } from '@/auth';
 
-export default function SideNav() {
+export default async function SideNav() {
+  const userData: any = await getUserdata();
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -24,6 +25,10 @@ export default function SideNav() {
             await signOut();
           }}
         >
+          
+          <div className="hidden md:flex items-center text-sm font-medium text-gray-600">
+            Personnel {userData?.name} from {userData?.department}
+          </div>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
