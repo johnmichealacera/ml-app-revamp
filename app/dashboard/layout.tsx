bde52441277cdd7b3512ac2afd36810eddf5a9dd
JM Acera <john.acera@sportality.se>
2024-06-07 16:24:08 +0800

    add header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { Metadata } from 'next';
+import HeaderNav from '../ui/dashboard/headernav';
  
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -7,11 +8,18 @@ export const metadata: Metadata = {
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <div className="flex flex-col h-screen">
+      <header className="flex-none">
+        <HeaderNav />
+      </header>
+      <div className="flex flex-grow overflow-hidden">
+        <aside className="w-full flex-none md:w-64">
+          <SideNav />
+        </aside>
+        <main className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </main>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 }
